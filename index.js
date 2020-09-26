12e954eb3793345004ee78270ba25a4ad0a494da
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-26 06:54:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const testVar = "hi"
 
 /* Enter the code to remove the node element here */
-document.querySelector
+document.querySelector("main").remove();
 
 /* Create your new element here and assign it to newHeader */
