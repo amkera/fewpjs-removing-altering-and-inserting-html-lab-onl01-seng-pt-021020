ad4fdee7bb1ca410772cb56830178f82f50015e9
Amber Kerawala Torres <amber_kerawala@berkeley.edu>
2020-09-26 06:52:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,5 @@
 
 //Make sure you remove the <main> with id 'main': expected <main id="main"></main> to not exist
 
-const main = document.createElement("main")
-
+main.setAttribute("id", "main");
 //has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
